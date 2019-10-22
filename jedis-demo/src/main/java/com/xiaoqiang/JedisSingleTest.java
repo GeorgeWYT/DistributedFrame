8241f62d2d844e6382b45285e6fa73dfe4293776
GeorgeWYT <459792531@qq.com>
2019-10-22 16:36:20 +0800

    增加redis demo

---
+++ jedis-demo/src/main/java/com/xiaoqiang/JedisSingleTest.java
@@ -0,0 +1,29 @@
+package com.xiaoqiang;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Jedis 简单使用
+ * @author George on 2019-10-22
+ **/
+public class JedisSingleTest {
+    public static void main(String[] args) {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(20);
+        jedisPoolConfig.setMaxIdle(20);
+        jedisPoolConfig.setMinIdle(20);
+
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, "127.0.0.1", 6379, 3000, null);
+
+        Jedis jedis;
+        try {
+            jedis = jedisPool.getResource();
+            jedis.set("single", "xiaoqiang");
+            System.out.println(jedis.get("single"));
+        }catch (Exception e){
+
+        }
+    }
+}
